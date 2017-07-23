711560f25652a1def7cee3820007ef3a81de2a59
torrent benoit <torrentdu64@gmail.com>
2017-07-23 14:17:59 +0200

    add asset scss and meta flash layout ...

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,19 @@
+class MainController < ApplicationController
+  def home
+  end
+
+  def categories
+  end
+
+  def products
+  end
+
+  def details
+  end
+
+  def panier
+  end
+
+  def geo
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'main#home'
+
+  get 'main/categories'
+
+  get 'main/products'
+
+  get 'main/details'
+
+  get 'main/panier'
+
+  get 'main/geo'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
